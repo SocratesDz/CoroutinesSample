dfbfd647e3c3036fbce78a4a0aca47757ace0fe4
Sócrates Díaz <sdsxd1800@gmail.com>
2021-05-09 16:09:59 -0400

    Initial commit. Home fragment layout created.

---
+++ app/src/main/java/com/example/coroutinesamples/HomeFragment.kt
@@ -0,0 +1,31 @@
+package com.example.coroutinesamples
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.example.coroutinesamples.databinding.FragmentHomeBinding
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class HomeFragment : Fragment() {
+
+    lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
